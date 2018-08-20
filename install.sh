79fd15aebccba004100ca69acb2333482bd49f82
gitgyver <gitgyver@protonmail.com>
2018-08-20 02:26:28 +0200

    Initial commit

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ fwd="$bwd/eosfactory"
 cwd="$bwd/contracts"
 ewd="$bwd/eos"
 
-#git submodule update --init --recursive
+git submodule update --init --recursive
 
-#cd eos
-#./eosio_build.sh
-#cd build
-#sudo make install
-#cd ..
-#nodeos --extract-genesis-json genesis.json
+cd eos
+./eosio_build.sh
+cd build
+sudo make install
+cd ..
+nodeos --extract-genesis-json genesis.json
 
-#cd ..
+cd ..
 
 cd $fwd
 
